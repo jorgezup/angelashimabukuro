e814aeabd19403c668847f75e0af781e635ef15a
Jorge Zupirolli <jorgebertolo@gmail.com>
2021-03-04 08:27:29 -0400

    style header

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,32 +11,33 @@ const Header = ({ header }) => {
   const myLoader = ({ src, width, quality }) => {
     return `${src}?w=${width}&q=${quality || 75}`
   }
-
+  const backgroundImage = `${process.env.NEXT_PUBLIC_IMAGES_DOMAIN}/${header.background.hash}`
   return (
-    <Container >
-      <div className="background">
+    <Container image={backgroundImage}>
+      {/* <div className="background">
         <Image
           loader={myLoader}
           src={`${process.env.NEXT_PUBLIC_IMAGES_DOMAIN}/${header.background.hash}.webp`}
           layout="fill"
           objectFit="cover"
+          objectPosition="center"
         />
-      </div>
+      </div> */}
 
       <Wrapper>
-        <div className="text">
+        <div className="content">
           <MarkdownPreview value={header.text1} />
           <MarkdownPreview value={header.text2} />
+          <Button
+            url={header.button.url}
+            text={header.button.text}
+            color={header.button.buttoncolor}
+            textcolor={header.button.textcolor}
+          />
           <MarkdownPreview value={header.text3} />
           <MarkdownPreview value={header.text4} />
         </div>
 
-        <Button
-          url={header.button.url}
-          text={header.button.text}
-          color={header.button.buttoncolor}
-          textcolor={header.button.textcolor}
-        />
       </Wrapper>
     </Container >
   )
--- src/styles/components/Header.js
+++ src/styles/components/Header.js
@@ -1,56 +1,49 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-  /* height: 150vh; */
-  /* height: min(max(32rem, 100vh), 70rem); */
-
+  /* height: 100vh; */
   background: 
     linear-gradient(
       rgba(0, 0, 0, 0.65),
-      rgba(0, 0, 0, 0.65) 
+      rgba(0, 0, 0, 0.65)
     ),
+    url(${props => props.image})
+    center center  / 100vw 100vh
+    no-repeat
+    fixed
+    padding-box
     content-box
     ;
+  background-size: cover;
 
-  .background {
-    position: fixed;
-    height: 100vh;
-    width: 100vw;
-    z-index: -1;
-  }
-
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `
 
 export const Wrapper = styled.div`
-  width: clamp(300px, 96%, 1100px);
-  height: min(max(20rem, 100vh), 70rem);
-  /* height: 100%; */
+  width: clamp(300px, 96%, 960px);
+  /* height: min(max(20rem, 110vh), 80rem); */
+  /* margin: 10rem auto 0; */
+  padding: 4rem 0 2rem;
+  min-height: 100vh;
 
+  @media(min-width:768px) {
+    height: min(max(20rem, 110vh), 80rem);
+  }
+  
+  
 
-  margin: 6rem auto;
-
-
-  display: flex;
-  flex-direction: column;
-  /* justify-content: space-evenly; */
-
-  .text {
-    /* flex: 1; */
+  .content {
+    margin: 2rem 0 0;
     height: 100%;
-    /* height: min(max(20rem, 120vh), 70rem); */
+    /* padding: 2rem 0; */
 
     display: flex;
     flex-direction: column;
 
     justify-content: space-between;
-
-    /* margin-top: 8vh; */
-    /* height: 100%; */
-
-
-    /* font-size: 1.2rem; */
     font-size: min(max(1.7rem, 4vw), 2.4rem);
-    line-height: min(max(2.2rem, 4vw), 3.4rem);
 
     font-weight: 400;
     color: #fff;
@@ -67,23 +60,66 @@ export const Wrapper = styled.div`
     }
 
     div:first-of-type{
-      font-size: min(max(2rem, 4vw), 2.8rem);
-    }
+      font-size: min(max(2.8rem, 2vw), 7rem);
+      font-size: 3.5rem;
+      text-transform: uppercase;
+      word-wrap: wrap;
+      line-height: 1;
+      font-weight: 700;
+      text-align: center;
+      margin-bottom: 4rem;
+      @media(min-width:320px) {
+        font-size: 3rem;
+      }
 
-    div:nth-child(2) {
-      padding: 0 3rem;
-      font-weight: 300;
+      @media(min-width:425px) {
+        font-size: 3.2rem;
+      }
 
-      @media(min-width: 1024px) {
-        padding: 0 8rem;
+      @media(min-width:768px) {
+        font-size: 3.3rem;
       }
     }
 
-    div:nth-child(3){
-      font-weight: 700;
-      font-size: min(max(2rem, 4vw), 2.4rem);
+    div:nth-child(2) {
+      /* padding: 0 3rem; */
+      font-size:1.6rem;
+      font-weight: 300;
+      line-height: 1.2;
+      margin-bottom: 4rem;
+      text-align: center;
+
+      /* @media(min-width:320px) {
+        font-size: 1.3rem;
+      }
+
+      @media(min-width:425px) {
+        font-size: 3.2rem;
+      }
+
+      @media(min-width:768px) {
+        font-size: 3.3rem;
+      } */
+    }
+
+    div:nth-child(3) {
+      /* margin-bottom: 4rem; */
+      border-radius: 36px;
+      margin: initial;
+      margin: 0 auto;
+      font-size: min(max(1.54rem, 4vw), 2.4rem);
+      a {
+        font-weight: 600;
+      }
+      
+    }
+
+    div:nth-child(4){
+      font-weight: 300;
+      font-size: min(max(1.42rem, 4vw), 2.4rem);
+      line-height: 1;
+      font-size: 1.3rem;
+      margin-top: 2rem;
     }
   }
-
-  
 `
